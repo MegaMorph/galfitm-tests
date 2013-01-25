20db66078f07e62a6e5bf19056fdb8e371148d3d
Steven Bamford <steven@stevenbamford.com>
2013-01-25 14:23:28 +0000

    spiralsim test make_images.py now accepts architecture
    Former-commit-id: 891d827da2ac9760561278b526529f39bbffbc67

--- spiralsim/make_images.py
+++ spiralsim/make_images.py
@@ -4,9 +4,20 @@
 
 from glob import glob
 import pyfits
-import sys, os
+import os, sys, getopt
 
-def make_images():
+
+"""make_images.py - Create images for galfitm spiralsim test
+
+    Usage:
+        make_images.py [arch]
+        
+        where [arch] is 'linux' or 'osx',
+        defaulting to linux if not specified.
+
+"""
+
+def make_images(arch='linux'):
 
     noiselevels = [1, 5, 10, 50, 100]
     noise = [pyfits.getdata('n%i.fits'%i) for i in noiselevels]
@@ -14,13 +25,45 @@ def make_images():
     gals = glob('galfit.gal*')
 
     for g in gals:
-        os.system('galfitm-0.0.3 %s'%g)
+        os.system('galfitm-0.0.3-%s %s'%(arch, g))
         imgname = g.replace('galfit.', '')
         for i, n in enumerate(noiselevels):
             img = pyfits.open(imgname+'.fits')
             img[0].data += noise[i]
             img.writeto(imgname+'n%i.fits'%n, clobber=True)
 
-if __name__ =='__main__':
-    sys.exit(make_images())
 
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "hf", ["help", "force"])
+        except getopt.error, msg:
+            raise Usage(msg)
+        clobber = False
+        for o, a in opts:
+            if o in ("-h", "--help"):
+                print __doc__
+                return 1
+            if o in ("-f", "--force"):
+                clobber = True
+        if len(args) == 0:
+            arch = 'linux'
+        if len(args) == 1:
+            arch = args[0]
+        elif len(args) > 1:
+            raise Usage("Wrong number of arguments")
+        make_images(arch)
+    except Usage, err:
+        print >>sys.stderr, err.msg
+        print >>sys.stderr, "For help use --help"
+        return 2
+ 
+if __name__ == "__main__":
+    sys.exit(main())
